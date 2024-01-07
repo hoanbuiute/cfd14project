fe1eb9a0d42e6649b851eb35d613c38efbfd6fd9
hoanbuiute <155743575+hoanbuiute@users.noreply.github.com>
2024-01-07 17:07:52 +0700

    Update index.jsx

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import useQuery from "../../hooks/useQuery";
-import { courseService } from "../../services/CoursesSevice";
+import { courseService } from "../../services/coursesSevice";
 import { teamsService } from "../../services/teamsService";
 import { questionsService } from "../../services/questionsService";
 import { galleryService } from "../../services/galleryService";
